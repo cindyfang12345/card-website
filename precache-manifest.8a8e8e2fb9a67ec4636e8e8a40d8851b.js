9ff5ca78dbb2c83945fdd739845db6b9536fb17b
Cindy Fang <cindyfang12345@hotmail.com>
2020-01-01 22:22:22 -0500

    Updates

---
+++ precache-manifest.8a8e8e2fb9a67ec4636e8e8a40d8851b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c8f8f68fb4fcf3b2a5a66d07339885",
+    "url": "/card-website/index.html"
+  },
+  {
+    "revision": "845ce3245df1b9439de5",
+    "url": "/card-website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "696bc0a42c72515642a6",
+    "url": "/card-website/static/js/2.b4979256.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/card-website/static/js/2.b4979256.chunk.js.LICENSE"
+  },
+  {
+    "revision": "845ce3245df1b9439de5",
+    "url": "/card-website/static/js/main.f295db06.chunk.js"
+  },
+  {
+    "revision": "85dfff5ee8284e3e3ddd",
+    "url": "/card-website/static/js/runtime-main.2bd2f1e5.js"
+  }
+]);
